1e0da1c319083017e1e22ed6350ffc7e37875e59
Jean-Damien Durand <jeandamiendurand@free.fr>
2015-09-25 06:01:22 +0200

    dev+

--- lib/MarpaX/Languages/XML/Impl/WFC.pm
+++ lib/MarpaX/Languages/XML/Impl/WFC.pm
@@ -9,13 +9,18 @@ class MarpaX::Languages::XML::Impl::WFC {
   use MarpaX::Languages::XML::Role::WFC;
   use MooX::Object::Pluggable;
   use MooX::Options;
+  use MooX::Role::Logger;
 
   option wfc => ( is => 'rw', isa => Bool, default => true, short => 'w', negativable => 1, doc => q{Well-Formed constraints. Default to a true value. Option is negativable with '--no-' prefix.} );
 
   around BUILD {
-    $self->${^NEXT}(@_);
+    $self = $self->${^NEXT}(@_);
     my $pluginFactory = MarpaX::Languages::XML::Impl::PluginFactory->new(fromModule => __PACKAGE__);
-    my @plugins = $pluginFactory->load_plugins;
+    my $pluginsArrayRef = $pluginFactory->load_plugins;
+    foreach (@{$pluginsArrayRef}) {
+      print STDERR "$_->new() ?\n";
+      $_->new();
+    }
   }
 
   method _trigger_wfc (Bool $wfc, @rest --> Undef) {
