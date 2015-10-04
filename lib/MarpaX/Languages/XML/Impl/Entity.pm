a48016b0c144c7f01aa9748d4e77c9fbd882eb79
Jean-Damien Durand <jeandamiendurand@free.fr>
2015-10-04 12:48:24 +0200

    Revisit Entity generic implementation

--- lib/MarpaX/Languages/XML/Impl/Entity.pm
+++ lib/MarpaX/Languages/XML/Impl/Entity.pm
@@ -8,9 +8,9 @@ class MarpaX::Languages::XML::Impl::Entity {
   use MarpaX::Languages::XML::Role::Entity;
   use MarpaX::Languages::XML::Type::EntityType -all;
 
-  has name    => (is => 'ro', isa => Str|Undef,  required => 1); # document and external DTD subset have no name
-  has type    => (is => 'ro', isa => EntityType, required => 1);
-  has content => (is => 'ro', isa => Str,        required => 1);
+  has name    => (is => 'ro', isa => Maybe[Str], required => 1); # document and external DTD subset have no name
+  has type    => (is => 'ro', isa => EntityType, required => 1); # Parsed or Unparsed
+  has content => (is => 'ro', isa => Str,        required => 1); # Better than Value and will hold anything
 
   with qw/MarpaX::Languages::XML::Role::Entity/;
 }
